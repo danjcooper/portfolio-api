228888e6e65a0f7913b43fee42e8efa2dcb6ba8d
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-03-08 13:16:10 +0000

    response message updated

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -3,7 +3,7 @@ const portfolioModal = require('../models/portfolio');
 const addMessage = async (req, res) => {
   try {
     await portfolioModal.create(req.body);
-    res.status(200).send('Added');
+    res.status(200).send({ message: 'message has been sent' });
   } catch (error) {
     res.status(500).send(error.message);
   }
