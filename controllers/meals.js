eb2dcfd9c10f188ae772fb60c29d3f6c0e31e3e6
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-03-19 13:32:19 +0000

    update response status handling

--- controllers/meals.js
+++ controllers/meals.js
@@ -16,9 +16,11 @@ const getAllMeals = async (req, res) => {
 const updateMeals = async (req, res) => {
   try {
     const result = mealsModal.updateShoppingList(req.body);
+    res.status(200);
     res.send({ data: result });
     return;
   } catch (error) {
+    res.status(500);
     res.send(error.message);
     return;
   }
