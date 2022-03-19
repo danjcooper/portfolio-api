fb3ef0fcadabe6a3ce0a0af700d29565ce62a109
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-03-19 13:29:02 +0000

    update response code

--- controllers/meals.js
+++ controllers/meals.js
@@ -3,10 +3,14 @@ const mealsModal = require('../models/meals');
 const getAllMeals = async (req, res) => {
   try {
     const result = await mealsModal.all;
-    res.sendStatus(200).send({ data: result });
+    res.sendStatus(200);
+
+    res.send({ data: result });
     return;
   } catch (error) {
-    res.sendStatus(500).send(error.message);
+    res.sendStatus(500);
+
+    res.send(error.message);
     return;
   }
 };
